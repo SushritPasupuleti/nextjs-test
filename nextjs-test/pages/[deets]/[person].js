af2d7acd21edc0a612b5d469f066fdaf1b7fe997
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-29 13:28:54 +0530

    added Server Side fetch

--- nextjs-test/pages/[deets]/[person].js
+++ nextjs-test/pages/[deets]/[person].js
@@ -1,9 +1,10 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-export default function PersonDetails() {
+export default function PersonDetails(props) {
     const router = useRouter();
     console.log(router.query);
+    console.log("props:", props.randoUser)
 
     const [randomUser, setRandomUser] = React.useState();
 
@@ -11,7 +12,7 @@ export default function PersonDetails() {
         fetch(`https://randomuser.me/api/`)
           .then(response => response.json()) // parse JSON from request
           .then(resultData => {
-            setRandomUser(resultData.results[0].name.first)
+            setRandomUser(resultData.results[0].name.first + " " + resultData.results[0].name.last)
           })
     }
 
@@ -20,6 +21,17 @@ export default function PersonDetails() {
     }, [])
 
     return (
-        <h1>Yo from {randomUser} Details!</h1>
+    <h1>Yo from {randomUser} & {props.randoUser}</h1>
     )
+}
+
+PersonDetails.getInitialProps = async () => {
+    const getRandom = await
+        fetch(`https://randomuser.me/api/`)
+          .then(response => response.json()) // parse JSON from request
+          .then(resultData => {
+            return{randoUser :resultData.results[0].name.first + " " + resultData.results[0].name.last}
+          })
+
+    return await getRandom
 }
