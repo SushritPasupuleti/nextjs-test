090ad6792b534c35768748837d9ee48adecb4fb4
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-29 10:48:16 +0530

    added basic data fetch

--- nextjs-test/pages/[deets]/[person].js
+++ nextjs-test/pages/[deets]/[person].js
@@ -1,10 +1,25 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function PersonDetails() {
     const router = useRouter();
     console.log(router.query);
 
+    const [randomUser, setRandomUser] = React.useState();
+
+    const getRandom = () => {
+        fetch(`https://randomuser.me/api/`)
+          .then(response => response.json()) // parse JSON from request
+          .then(resultData => {
+            setRandomUser(resultData.results[0].name.first)
+          })
+    }
+
+    useEffect(() => {
+        getRandom()
+    }, [])
+
     return (
-        <h1>Yo from {router.query.deets} Details!</h1>
+        <h1>Yo from {randomUser} Details!</h1>
     )
 }
