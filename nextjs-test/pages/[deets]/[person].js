1a4efdbc39eb4a656c353d31e68d33f4c2d381e4
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-29 18:53:28 +0530

    Added basic material-ui card

--- nextjs-test/pages/[deets]/[person].js
+++ nextjs-test/pages/[deets]/[person].js
@@ -1,7 +1,33 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+        margin: '2rem'
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
 
 export default function PersonDetails(props) {
+    const classes = useStyles();
+
     const router = useRouter();
     console.log(router.query);
     console.log("props:", props.randoUser)
@@ -10,10 +36,10 @@ export default function PersonDetails(props) {
 
     const getRandom = () => {
         fetch(`https://randomuser.me/api/`)
-          .then(response => response.json()) // parse JSON from request
-          .then(resultData => {
-            setRandomUser(resultData.results[0].name.first + " " + resultData.results[0].name.last)
-          })
+            .then(response => response.json()) // parse JSON from request
+            .then(resultData => {
+                setRandomUser(resultData.results[0].name.first + " " + resultData.results[0].name.last)
+            })
     }
 
     useEffect(() => {
@@ -21,19 +47,30 @@ export default function PersonDetails(props) {
     }, [])
 
     return (
-    <h1>Yo from {randomUser} & {props.randoUser}</h1>
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography className={classes.title} color="textPrimary" gutterBottom>
+                    Yo from {randomUser} & {props.randoUser}
+                </Typography>
+                <h1></h1>
+            </CardContent>
+        </Card>
     )
 }
 
 PersonDetails.getInitialProps = async (ctx) => { //context to persist data from main component definition
-    const {query} = ctx;
+    if (!ctx.req) {
+        return { randoUser: '' }
+    }
+
+    const { query } = ctx;
     console.log("ctx: ", query)
     const getRandom = await
         fetch(`https://randomuser.me/api/`)
-          .then(response => response.json()) // parse JSON from request
-          .then(resultData => {
-            return{randoUser :resultData.results[0].name.first + " " + resultData.results[0].name.last}
-          })
+            .then(response => response.json()) // parse JSON from request
+            .then(resultData => {
+                return { randoUser: resultData.results[0].name.first + " " + resultData.results[0].name.last }
+            })
 
     return getRandom
 }
