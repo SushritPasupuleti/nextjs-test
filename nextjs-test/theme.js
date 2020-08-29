0368d23440655b5a2b7178670a010a0c3e46c75e
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-29 19:21:20 +0530

    Update theme.js

--- nextjs-test/theme.js
+++ nextjs-test/theme.js
@@ -3,21 +3,22 @@ import { red } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: '#556cd6',
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            paper: '#424242',
+            default: '#303030',
+        },
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
 });
 
 export default theme;
