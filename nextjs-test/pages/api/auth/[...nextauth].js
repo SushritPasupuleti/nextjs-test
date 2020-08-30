ba132706e50a5b2d0ffc395248434c6d1ae3580e
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-30 22:21:03 +0530

    Update [...nextauth].js

--- nextjs-test/pages/api/auth/[...nextauth].js
+++ nextjs-test/pages/api/auth/[...nextauth].js
@@ -1,105 +1,118 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
 const options = {
-  site: process.env.VERCEL_URL,
+  // https://next-auth.js.org/configuration/providers
   providers: [
     // Providers.Email({
-    //   // SMTP connection string or nodemailer configuration object https://nodemailer.com/
-    //   server: process.env.EMAIL_SERVER,
-    //   // Email services often only allow sending email from a valid/verified address
+    //   server: process.env.EMAIL_SERVER, 
     //   from: process.env.EMAIL_FROM,
     // }),
-    // When configuring oAuth providers make sure you enabling requesting
-    // permission to get the users email address (required to sign in)
-    Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
+    // Providers.Apple({
+    //   clientId: process.env.APPLE_ID,
+    //   clientSecret: { 
+    //     appleId: process.env.APPLE_ID,
+    //     teamId: process.env.APPLE_TEAM_ID,
+    //     privateKey: process.env.APPLE_PRIVATE_KEY,
+    //     keyId: process.env.APPLE_KEY_ID,
+    //   }
+    // }),
+    // Providers.Auth0({
+    //   clientId: process.env.AUTH0_ID,
+    //   clientSecret: process.env.AUTH0_SECRET,
+    //   domain: process.env.AUTH0_DOMAIN
+    // }),
     // Providers.Facebook({
     //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET,
-    // }),
-    // Providers.Twitter({
-    //   clientId: process.env.TWITTER_ID,
-    //   clientSecret: process.env.TWITTER_SECRET,
+    //   clientSecret: process.env.FACEBOOK_SECRET
     // }),
     // Providers.GitHub({
     //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
+    //   clientSecret: process.env.GITHUB_SECRET
+    // }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+    // Providers.Twitter({
+    //   clientId: process.env.TWITTER_ID,
+    //   clientSecret: process.env.TWITTER_SECRET
     // }),
   ],
-  // The 'database' option should be a connection string or TypeORM
-  // configuration object https://typeorm.io/#/connection-options
+  // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
+  // https://next-auth.js.org/configuration/database
   //
   // Notes:
-  // * You need to install an appropriate node_module for your database!
-  // * The email sign in provider requires a database but OAuth providers do not
-  //database: process.env.DATABASE_URL,
+  // * You must to install an appropriate node_module for your database
+  // * The Email provider requires a database (OAuth providers do not)
+  database: process.env.DATABASE_URL,
+
+  // The secret should be set to a reasonably long random string.
+  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
+  // a seperate secret is defined explicitly for encrypting the JWT.
+  secret: process.env.SECRET,
 
   session: {
     // Use JSON Web Tokens for session instead of database sessions.
     // This option can be used with or without a database for users/accounts.
     // Note: `jwt` is automatically set to `true` if no database is specified.
-    // jwt: false,
+    jwt: true, 
+    
     // Seconds - How long until an idle session expires and is no longer valid.
     // maxAge: 30 * 24 * 60 * 60, // 30 days
+
     // Seconds - Throttle how frequently to write to database to extend a session.
     // Use it to limit write operations. Set to 0 to always update the database.
-    // Note: This option is ignored if using JSON Web Tokens
+    // Note: This option is ignored if using JSON Web Tokens 
     // updateAge: 24 * 60 * 60, // 24 hours
-    // Easily add custom properties to response from `/api/auth/session`.
-    // Note: This should not return any sensitive information.
-    /*
-    get: async (session) => {
-      session.customSessionProperty = "ABC123"
-      return session
-    }
-    */
   },
 
-  // JSON Web Token options
+  // JSON Web tokens are only used for sessions if the `jwt: true` session
+  // option is set - or by default if no database is specified.
+  // https://next-auth.js.org/configuration/options#jwt
   jwt: {
-    // secret: 'my-secret-123', // Recommended (but auto-generated if not specified)
-    // Custom encode/decode functions for signing + encryption can be specified.
-    // if you want to override what is in the JWT or how it is signed.
-    // encode: async ({ secret, key, token, maxAge }) => {},
-    // decode: async ({ secret, key, token, maxAge }) => {},
-    // Easily add custom to the JWT. It is updated every time it is accessed.
-    // This is encrypted and signed by default and may contain sensitive information
-    // as long as a reasonable secret is defined.
-    /*
-    set: async (token) => {
-      token.customJwtProperty = "ABC123"
-      return token
-    }
-    */
+    // A secret to use for key generation (you should set this explicitly)
+    // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw', 
+    
+    // Set to true to use encryption (default: false)
+    // encryption: true,
+
+    // You can define your own encode/decode functions for signing and encryption
+    // if you want to override the default behaviour.
+    // encode: async ({ secret, token, maxAge }) => {},
+    // decode: async ({ secret, token, maxAge }) => {},
   },
 
-  // Control which users / accounts can sign in
-  // You can use this option in conjunction with OAuth and JWT to control which
-  // accounts can sign in without having to use a database.
-  allowSignin: async (user, account) => {
-    // Return true if user / account is allowed to sign in.
-    // Return false to display an access denied message.
-    return true
-  },
-
-  // You can define custom pages to override the built-in pages
-  // The routes shown here are the default URLs that will be used.
+  // You can define custom pages to override the built-in pages.
+  // The routes shown here are the default URLs that will be used when a custom
+  // pages is not specified for that route.
+  // https://next-auth.js.org/configuration/pages
   pages: {
-    // signin: '/api/auth/signin',  // Displays signin buttons
-    // signout: '/api/auth/signout', // Displays form with sign out button
+    // signIn: '/api/auth/signin',  // Displays signin buttons
+    // signOut: '/api/auth/signout', // Displays form with sign out button
     // error: '/api/auth/error', // Error code passed in query string as ?error=
     // verifyRequest: '/api/auth/verify-request', // Used for check email page
     // newUser: null // If set, new users will be directed here on first sign in
   },
 
-  // Additional options
-  // secret: 'abcdef123456789' // Recommended (but auto-generated if not specified)
-  // debug: true, // Use this option to enable debug messages in the console
+  // Callbacks are asynchronous functions you can use to control what happens
+  // when an action is performed.
+  // https://next-auth.js.org/configuration/callbacks 
+  callbacks: { 
+    // signIn: async (user, account, profile) => { return Promise.resolve(true) },
+    // redirect: async (url, baseUrl) => { return Promise.resolve(baseUrl) },
+    // session: async (session, user) => { return Promise.resolve(session) },
+    // jwt: async (token, user, account, profile, isNewUser) => { return Promise.resolve(token) }
+  },
+
+  // Events are useful for logging
+  // https://next-auth.js.org/configuration/events
+  events: { },
+
+  // Enable debug messages in the console if you are having problems
+  debug: false,
 }
 
-const Auth = (req, res) => NextAuth(req, res, options)
-
-export default Auth
+export default (req, res) => NextAuth(req, res, options)
