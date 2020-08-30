6d31cc2becc3bcd70ad8ca07d122b9dca7687fe4
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-30 18:36:51 +0530

    Update _app.js

--- nextjs-test/pages/_app.js
+++ nextjs-test/pages/_app.js
@@ -7,12 +7,14 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../theme';
 
+import { Provider as NextAuthProvider } from 'next-auth/client'
 import { Provider } from 'react-redux'
 import { useStore } from '../store'
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
   const store = useStore(pageProps.initialReduxState)
+  const { session } = pageProps
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -24,17 +26,19 @@ export default function MyApp(props) {
 
   return (
     <Provider store={store}>
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
+      <NextAuthProvider options={{ site: process.env.SITE }} session={session}>
+        <React.Fragment>
+          <Head>
+            <title>My page</title>
+            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          </Head>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </React.Fragment>
+      </NextAuthProvider>
     </Provider>
   );
 }
