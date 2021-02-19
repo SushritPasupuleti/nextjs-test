4497dd485328cc19300d9f36bd713a95a636d2fe
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-02-19 18:47:28 +0530

    create mock data served over express

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello there')
+})
+
+app.post('/data', (req, res) => {
+  res.json(
+    [
+        {"title": "wow", "body": "wow body"},
+        {"title": "wow1", "body": "wow body1"},
+        {"title": "wow2", "body": "wow body2"},
+        {"title": "wow3", "body": "wow body3"},
+    ])
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
