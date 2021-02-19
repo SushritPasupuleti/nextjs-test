3694910355f6faadc76dc1828b4cf70a5eead6f6
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-02-19 23:08:47 +0530

    fix bug that tried to resend data from server

--- server/server.js
+++ server/server.js
@@ -2,36 +2,35 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-posts = 
-[
-    {"title": "wow", "body": "wow body"},
-    {"title": "wow1", "body": "wow body1"},
-    {"title": "wow2", "body": "wow body2"},
-    {"title": "wow3", "body": "wow body3"},
-]
+posts =
+    [
+        { "title": "wow", "body": "wow body" },
+        { "title": "wow1", "body": "wow body1" },
+        { "title": "wow2", "body": "wow body2" },
+        { "title": "wow3", "body": "wow body3" },
+    ]
 
 app.get('/', (req, res) => {
-  res.send('Hello there')
+    res.send('Hello there')
 })
 
 app.get('/data', (req, res) => {
     console.log("Data Request")
-    res.json(posts)
+    res.status(200).json(posts)
 })
 
 app.get('/data/:id', (req, res) => {
     console.log(`Specfic Data Request, ${req.params.id}`)
-    
-    for (post in posts){
+
+    for (post in posts) {
         console.log(posts[post]['title'])
-        if (posts[post]['title'] === req.params.id) 
-        {
-            res.json(posts[post])
+        if (posts[post]['title'] === req.params.id) {
+            return res.status(200).json(posts[post])
         }
     }
-    return res.json({"success": "false"})
+    return res.status(200).json({ "success": "false" })
 })
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
+    console.log(`Server listening at http://localhost:${port}`)
 })
